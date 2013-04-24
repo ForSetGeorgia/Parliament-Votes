e4c0efa41506836246a977ed5ba1775e170c9bae
jason <jason.addie@gmail.com>
2013-04-25 00:21:24 +0400

    added prod deploy

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,10 +1,10 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/bootstrap/Bootstrap-Starter/current" # path to application current folder
-sock_name = "unicorn_Bootstrap-Starter" # must be unique name with no '.'
-port_num = 8091 # must be a unique port number for this application
-tout = 30 # time in seconds before process dies - need a long time for data uploads
+root = "/home/vrd/Voting-Records/current" # path to application current folder
+sock_name = "unicorn_Voting-Records" # must be unique name with no '.'
+port_num = 8107 # must be a unique port number for this application
+tout = 300 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
