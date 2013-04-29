d4f8820627629f5f7394c37f48bb4ec8a2c49433
jason <jason.addie@gmail.com>
2013-04-29 19:59:57 +0400

    fixed popup so datatables work in popup

--- app/assets/javascripts/fancybox/layout.js
+++ app/assets/javascripts/fancybox/layout.js
@@ -1,12 +1,2 @@
-//= require i18n
-//= require i18n/translations
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require dataTables/jquery.dataTables
-//= require dataTables/jquery.dataTables.bootstrap
-//= require fancybox
-//= require fancybox2
-//= require vendor
 //= require fancybox/search
 
