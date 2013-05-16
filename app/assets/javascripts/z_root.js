a3d689bf87c5aea97a94439a366711d7b39c828d
jason <jason.addie@gmail.com>
2013-05-16 09:06:09 +0400

    added trans; styled placeholder text

--- app/assets/javascripts/z_root.js
+++ app/assets/javascripts/z_root.js
@@ -25,18 +25,6 @@ $(document).ready(function(){
         changeYear: true,
         yearRange: "2010:2013",
         changeMonth: true,
-				onClose: function(dateText, inst) {
-				    var startDateTextBox = $('#start_date');
-				    if (startDateTextBox.val() != '') {
-				        var testStartDate = new Date(startDateTextBox.val());
-				        var testEndDate = new Date(dateText);
-				        if (testStartDate > testEndDate)
-				            startDateTextBox.val(dateText);
-				    }
-				    else {
-				        startDateTextBox.val(dateText);
-				    }
-				},
 				onSelect: function (selectedDateTime){
 				    var end = $(this).datetimepicker('getDate');
 				    $('#start_date').datetimepicker('option', 'maxDate', new Date(end.getTime()) );
