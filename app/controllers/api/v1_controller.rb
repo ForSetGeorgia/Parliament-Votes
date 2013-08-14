1da34a1b74f9964993c0c3630019ab73781afade
jason <jason.addie@gmail.com>
2013-08-14 10:33:41 +0400

    bumped up # of unicorn users

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -55,8 +55,6 @@ protected
 
   # record call to google analytics
   def record_analytics(api_method)
-Rails.logger.info "referer = #{request.env['HTTP_REFERER']}"
-Rails.logger.info "ip = #{request.remote_ip}"
     ga_id = nil
     domain = nil
     if Rails.env.production?
