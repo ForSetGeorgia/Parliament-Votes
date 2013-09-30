d29658dea632bed3fef1ca501f2799d1eb928f3c
jason <jason.addie@gmail.com>
2013-09-30 22:56:10 +0400

    changing prod settings

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -4,7 +4,7 @@
 root = "/home/vrd/Voting-Records/current" # path to application current folder
 sock_name = "unicorn_Voting-Records" # must be unique name with no '.'
 port_num = 8107 # must be a unique port number for this application
-tout = 300 # time in seconds before process dies - need a long time for data uploads
+tout = 600 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
@@ -14,7 +14,7 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/#{sock_name}.sock"
 listen port_num, :tcp_nopush => true
-worker_processes 5
+worker_processes 4
 timeout tout
 
 #################################
