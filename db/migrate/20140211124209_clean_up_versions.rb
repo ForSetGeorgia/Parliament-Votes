c7d91ba73723fd34fe97fc071af6fee57bcf0fd9
jason <jason.addie@gmail.com>
2014-02-14 14:17:11 +0400

    updated version script to also clean up voting sessions, resluts and delegates

--- db/migrate/20140211124209_clean_up_versions.rb
+++ db/migrate/20140211124209_clean_up_versions.rb
@@ -2,24 +2,31 @@ class CleanUpVersions < ActiveRecord::Migration
   def up
     Version.transaction do 
       version_ids = []
-    
+      start = Time.now
+
+      puts "+++++++++++++++++++++++++++++++++"
+      puts "+++++++++++++++++++++++++++++++++"
+      puts "start time = #{start}"
+      puts "+++++++++++++++++++++++++++++++++"
+      puts "+++++++++++++++++++++++++++++++++"
+
       ########################
       # agendas
       ########################
       puts "- testing agendas"
-      Agenda.all.each do |record|
-        puts "----------------------"
-        puts "----------------------"
-        puts "----------------------"
+      Agenda.find_each do |record|
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "----------------------"
 #        puts "-- record: #{record.attributes.except('updated_at', 'impressions_count').inspect}"
         updates = record.versions.select{|x| x.event == 'update'}
-        if updates.present?
-          puts "--- record #{record.id} has #{updates.length} version update records"
+        if updates.present? && updates.length > 1
+#          puts "--- record #{record.id} has #{updates.length} version update records"
           
           update_ids = []
           
           # check for duplicate versions against current version, ignoring count changes
-          puts "--- checking for duplicates with current version"
+#          puts "--- checking for duplicates with current version"
           (updates.length-1).downto(0) do |index|
             begin 
               previous_record = updates[index].reify
@@ -43,7 +50,7 @@ class CleanUpVersions < ActiveRecord::Migration
           
           # now check for duplicates between old versions (not compared with current version as above)
           # - only check records that were not found as a repeat above
-          puts "--- checking for duplicates among old versions"
+#          puts "--- checking for duplicates among old versions"
           updates = updates.select{|x| update_ids.index(x.id).nil?}
           if updates.present? && updates.length > 1
             (updates.length-1).downto(1) do |index|
@@ -75,25 +82,27 @@ class CleanUpVersions < ActiveRecord::Migration
 
       puts "*********************************"
       puts "*********************************"
+      puts "** #{version_ids.length} records set for deletion so far; time so far = #{Time.now-start} seconds"
+      puts "*********************************"
       puts "*********************************"
     
       ########################
       # all delegates
       ########################
       puts "- testing all delegates"
-      AllDelegate.all.each do |record|
-        puts "----------------------"
-        puts "----------------------"
-        puts "----------------------"
+      AllDelegate.find_each do |record|
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "----------------------"
 #        puts "-- record: #{record.inspect}"
         updates = record.versions.select{|x| x.event == 'update'}
-        if updates.present?
-          puts "--- record #{record.id} has #{updates.length} version update records"
+        if updates.present? && updates.length > 1
+#          puts "--- record #{record.id} has #{updates.length} version update records"
 
           update_ids = []
           
           # check for duplicate versions against current version, ignoring count changes
-          puts "--- checking for duplicates with current version"
+#          puts "--- checking for duplicates with current version"
           (updates.length-1).downto(0) do |index|
             begin 
               previous_record = updates[index].reify
@@ -118,7 +127,7 @@ class CleanUpVersions < ActiveRecord::Migration
           
           # now check for duplicates between old versions (not compared with current version as above)
           # - only check records that were not found as a repeat above
-          puts "--- checking for duplicates among old versions"
+#          puts "--- checking for duplicates among old versions"
           updates = updates.select{|x| update_ids.index(x.id).nil?}
           if updates.present? && updates.length > 1
             (updates.length-1).downto(1) do |index|
@@ -146,6 +155,247 @@ class CleanUpVersions < ActiveRecord::Migration
           end
         end
       end
+
+
+      puts "*********************************"
+      puts "*********************************"
+      puts "** #{version_ids.length} records set for deletion so far; time so far = #{Time.now-start} seconds"
+      puts "*********************************"
+      puts "*********************************"
+    
+
+      ########################
+      # delegates
+      ########################
+      puts "- testing delegates"
+      Delegate.find_each do |record|
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "-- record: #{record.inspect}"
+        updates = record.versions.select{|x| x.event == 'update'}
+        if updates.present? && updates.length > 1
+#          puts "--- record #{record.id} has #{updates.length} version update records"
+
+          update_ids = []
+          
+          # check for duplicate versions against current version, ignoring count changes
+#          puts "--- checking for duplicates with current version"
+          (updates.length-1).downto(0) do |index|
+            begin 
+              previous_record = updates[index].reify
+  #            puts "--- previous record: #{previous_record.inspect}"
+              if record.attributes.except('updated_at') ==
+                  previous_record.attributes.except('updated_at')
+                
+  #              puts "---- **** found a repeat: #{updates[index].id}"
+                # found a repeat
+                version_ids << updates[index].id
+                update_ids << updates[index].id 
+              end
+            rescue Psych::SyntaxError
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "could not reify version so skipping"
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "+++++++++++++++++++++++++++++++++"
+            end
+          end
+
+          
+          # now check for duplicates between old versions (not compared with current version as above)
+          # - only check records that were not found as a repeat above
+#          puts "--- checking for duplicates among old versions"
+          updates = updates.select{|x| update_ids.index(x.id).nil?}
+          if updates.present? && updates.length > 1
+            (updates.length-1).downto(1) do |index|
+              begin 
+                record = updates[index].reify
+                (index-1).downto(0) do |index2|
+                  previous_record = updates[index2].reify
+                  
+                  if record.attributes.except('updated_at') ==
+                      previous_record.attributes.except('updated_at')
+                    
+  #                  puts "---- **** found a repeat among old versions: #{updates[index2].id}"
+                    # found a repeat
+                    version_ids << updates[index2].id
+                  end
+                end
+              rescue Psych::SyntaxError
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "could not reify version so skipping"
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "+++++++++++++++++++++++++++++++++"
+              end
+            end
+          end
+        end
+      end
+
+
+      puts "*********************************"
+      puts "*********************************"
+      puts "** #{version_ids.length} records set for deletion so far; time so far = #{Time.now-start} seconds"
+      puts "*********************************"
+      puts "*********************************"
+    
+
+      ########################
+      # voting results
+      ########################
+      puts "- testing voting results"
+      VotingResult.find_each do |record|
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "-- record: #{record.inspect}"
+        updates = record.versions.select{|x| x.event == 'update'}
+        if updates.present? && updates.length > 1
+#          puts "--- record #{record.id} has #{updates.length} version update records"
+
+          update_ids = []
+          
+          # check for duplicate versions against current version, ignoring count changes
+#          puts "--- checking for duplicates with current version"
+          (updates.length-1).downto(0) do |index|
+            begin 
+              previous_record = updates[index].reify
+  #            puts "--- previous record: #{previous_record.inspect}"
+              if record.attributes.except('updated_at') ==
+                  previous_record.attributes.except('updated_at')
+                
+  #              puts "---- **** found a repeat: #{updates[index].id}"
+                # found a repeat
+                version_ids << updates[index].id
+                update_ids << updates[index].id 
+              end
+            rescue Psych::SyntaxError
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "could not reify version so skipping"
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "+++++++++++++++++++++++++++++++++"
+            end
+          end
+
+          
+          # now check for duplicates between old versions (not compared with current version as above)
+          # - only check records that were not found as a repeat above
+#          puts "--- checking for duplicates among old versions"
+          updates = updates.select{|x| update_ids.index(x.id).nil?}
+          if updates.present? && updates.length > 1
+            (updates.length-1).downto(1) do |index|
+              begin 
+                record = updates[index].reify
+                (index-1).downto(0) do |index2|
+                  previous_record = updates[index2].reify
+                  
+                  if record.attributes.except('updated_at') ==
+                      previous_record.attributes.except('updated_at')
+                    
+  #                  puts "---- **** found a repeat among old versions: #{updates[index2].id}"
+                    # found a repeat
+                    version_ids << updates[index2].id
+                  end
+                end
+              rescue Psych::SyntaxError
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "could not reify version so skipping"
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "+++++++++++++++++++++++++++++++++"
+              end
+            end
+          end
+        end
+      end
+
+      
+      puts "*********************************"
+      puts "*********************************"
+      puts "** #{version_ids.length} records set for deletion so far; time so far = #{Time.now-start} seconds"
+      puts "*********************************"
+      puts "*********************************"
+      
+      
+      ########################
+      # voting sessions
+      ########################
+      puts "- testing voting sessions"
+      VotingSession.find_each do |record|
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "----------------------"
+#        puts "-- record: #{record.inspect}"
+        updates = record.versions.select{|x| x.event == 'update'}
+        if updates.present? && updates.length > 1
+#          puts "--- record #{record.id} has #{updates.length} version update records"
+
+          update_ids = []
+          
+          # check for duplicate versions against current version, ignoring count changes
+#          puts "--- checking for duplicates with current version"
+          (updates.length-1).downto(0) do |index|
+            begin 
+              previous_record = updates[index].reify
+  #            puts "--- previous record: #{previous_record.inspect}"
+              if record.attributes.except('updated_at') ==
+                  previous_record.attributes.except('updated_at')
+                
+  #              puts "---- **** found a repeat: #{updates[index].id}"
+                # found a repeat
+                version_ids << updates[index].id
+                update_ids << updates[index].id 
+              end
+            rescue Psych::SyntaxError
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "could not reify version so skipping"
+              puts "+++++++++++++++++++++++++++++++++"
+              puts "+++++++++++++++++++++++++++++++++"
+            end
+          end
+
+          
+          # now check for duplicates between old versions (not compared with current version as above)
+          # - only check records that were not found as a repeat above
+#          puts "--- checking for duplicates among old versions"
+          updates = updates.select{|x| update_ids.index(x.id).nil?}
+          if updates.present? && updates.length > 1
+            (updates.length-1).downto(1) do |index|
+              begin 
+                record = updates[index].reify
+                (index-1).downto(0) do |index2|
+                  previous_record = updates[index2].reify
+                  
+                  if record.attributes.except('updated_at') ==
+                      previous_record.attributes.except('updated_at')
+                    
+  #                  puts "---- **** found a repeat among old versions: #{updates[index2].id}"
+                    # found a repeat
+                    version_ids << updates[index2].id
+                  end
+                end
+              rescue Psych::SyntaxError
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "could not reify version so skipping"
+                puts "+++++++++++++++++++++++++++++++++"
+                puts "+++++++++++++++++++++++++++++++++"
+              end
+            end
+          end
+        end
+      end
+      
+
+      puts "*********************************"
+      puts "*********************************"
+      puts "** #{version_ids.length} records set for deletion so far; time so far = #{Time.now-start} seconds"
+      puts "*********************************"
+      puts "*********************************"
 
 
       # delete unnecessary versions
