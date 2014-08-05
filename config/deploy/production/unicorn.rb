3a341da745ece4f2acbd2311786b930b4da381c9
jason <jason.addie@gmail.com>
2014-08-05 15:15:07 +0400

    helps if press save

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,7 +1,7 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/vrd/Voting-Records/current" # path to application current folder
+root = "/home/zura/Voting-Records/current" # path to application current folder
 sock_name = "unicorn_Voting-Records" # must be unique name with no '.'
 port_num = 8107 # must be a unique port number for this application
 tout = 2500 # time in seconds before process dies - need a long time for data uploads
