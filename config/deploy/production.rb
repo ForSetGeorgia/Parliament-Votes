78ee5a0a25f4495816c20b92cc344eeecfcdd587
jason <jason.addie@gmail.com>
2017-06-30 12:10:49 +0400

    bug fix - repo name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ set :user, "deploy"# name of user on server# set :user, "zura"# name of user on 
 set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "production/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "ForSetGeorgia" # name of accout on git hub
-set :github_repo_name, "Parliament-Voting" # name of git hub repo
+set :github_repo_name, "Parliament-Votes" # name of git hub repo
 set :git_branch_name, "master" # name of branch to deploy
 set :rails_env, "production" # name of environment: production, staging, ...
 ##################################
