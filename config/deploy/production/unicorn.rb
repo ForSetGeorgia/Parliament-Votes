8575215c27616751a830462e066cebc84fbfc4b2
jason <jason.addie@gmail.com>
2017-06-30 11:32:27 +0400

    update prod deploy

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,7 +1,7 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/vrd/Voting-Records/current" # path to application current folder
+root = "/home/deploy/Voting-Records/current" # path to application current folder
 sock_name = "unicorn_Voting-Records" # must be unique name with no '.'
 port_num = 8107 # must be a unique port number for this application
 tout = 2500 # time in seconds before process dies - need a long time for data uploads
@@ -14,7 +14,7 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/#{sock_name}.sock"
 listen port_num, :tcp_nopush => true
-worker_processes 3
+worker_processes 1
 timeout tout
 
 #################################
