546b7840046e6928f8c1f1f29dec7e5b763369de
jason <jason.addie@gmail.com>
2018-01-29 15:25:30 +0400

    moving prod server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-server "pins.ge", :web, :app, :db, primary: true # server where app is located
+server "gamma.forset.ge", :web, :app, :db, primary: true # server where app is located
 set :application, "Voting-Records" # unique name of application
 set :user, "deploy"# name of user on server# set :user, "zura"# name of user on server
 set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
